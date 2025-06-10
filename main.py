ce2f2cc430b4b2227f061bf0bfa80cf6de9b2e96
Thaísa Vieira <thaisa-vieira@outlook.com.br>
2025-06-09 23:23:59 -0300

    add of attack and defense system

--- main.py
+++ main.py
@@ -63,12 +63,14 @@ def start_mini_game():
 def drawn_opponents(jujuba_lv):
    #opponents' list: [name, hp, strength, xp]
    mosquito = ['mosquito', 5,1,1]
-   cockroach = ['cockroach',10,2,2]
-   rat = ['rat', 15, 5, 10]
+   cockroach = ['cockroach',8,2,2]
+   rat = ['rat', 10, 5, 10]
    fish = ['fish', 15, 3, 15]
    bird = ['bird', 20, 4, 20]
    chicken = ['chicken', 30, 7, 6, 30]
    snake = ['snake',50, 10, 50]
+   
+   
    
    if jujuba_lv < 5:
       opponent_drawn = random.choice([mosquito, cockroach])
@@ -84,14 +86,39 @@ def game_over():
    print('You were a great Jujuba!')
    exit(0)
    
+def attack(attacker_name, attacker_strengh, defender_name,defender_hp, defender_strengh):
+   attacker_luck = random.randint(0,6)
+   defender_luck = random.randint(0,6)
+   
+   if attacker_luck == 6:
+      print(f'O {attacker_name} hit critical attack.')
+   elif attacker_luck > 0:
+      print(f'O {attacker_name} hit critical attack')
+   else:
+      print(f'O {attacker_name} missed the blow')
+   
+   damage = attacker_strengh*attacker_luck - defender_strengh*defender_luck
+   
+   if damage > 0:
+      print(f'The {defender_name} suffered damage of {damage}')
+      defender_hp -= damage
+   else:
+      print(f'The {defender_name} did not suffer any damage!')
+   
+   if defender_hp <= 0:
+      print(f'The {defender_name} died!')
+      return 0
+   else:
+      return defender_hp
+   
    
 
 
 show_title()
-
 list_of_attributes = start_mini_game()
  
 print(list_of_attributes)
 
-opponent_drawn = drawn_opponents(6)
+opponent_drawn = drawn_opponents(1)
 print(opponent_drawn)
+game_over()
